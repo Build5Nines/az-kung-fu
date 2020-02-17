254553f83fd05b1bfb72773554402758a6763763
Dan Patrick <dan@clouddeltav.com>
2020-02-16 23:17:35 -0500

    trying different install methods

--- az-kung-fu-vm/InstallazKungfuVm.ps1
+++ az-kung-fu-vm/InstallazKungfuVm.ps1
@@ -32,12 +32,17 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; Install-Module -Name Az -Allow
 #Install Azure CLI for Windows
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
 
-#Install Ubuntu for WSL and
+#Enable WSL
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+
+#Download and Install Ubuntu
 Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
 Add-AppxPackage -Path ~/Ubuntu.appx
 
-#Enable WSL
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+# Run the distro once and have it install locally with root user, unset password
+RefreshEnv
+Ubuntu1804 install --root
+sudo apt-get update -y && curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 #Reboot
 Restart-Computer -Force
